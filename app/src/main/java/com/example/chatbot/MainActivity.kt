cc602c10d5b4602d5094c4319915767c459f4b41
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-01-02 20:50:51 +0530

    second

--- app/src/main/java/com/example/chatbot/MainActivity.kt
+++ app/src/main/java/com/example/chatbot/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chatbot
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,6 +16,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatbot.ui.theme.ChatBotTheme
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
+import java.lang.IllegalArgumentException
 
 class MainActivity : ComponentActivity() {
     private val client = OkHttpClient()
@@ -36,13 +49,58 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun getResponse(question: String, callback: (Any) -> Unit){
-    val api = "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"
+    private fun getApiKeyFromFile(filepath : String) : String{
+        val file = File(filepath)
+
+        if(!file.exists()){
+            throw IllegalArgumentException("File Does Not Exist: $filepath")
+        }
+        val buffRead = BufferedReader(FileReader(file))
+        val apiKey = buffRead.readLine().trim()
+        buffRead.close()
+
+        return apiKey
+    }
+
+    private fun getResponse(question: String, callback: (String) -> Unit){
+    val api = getApiKeyFromFile("C:/Users/Hp/Desktop/apiKeys/OpenAi1.txt")
     val post = "https://api.openai.com/v1/chat/completions"
 
     val requestBody = """
-        
+        {
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": "You are a helpful assistant."
+                    },
+                    {
+                        "role": "user",
+                        "content": "$question"
+                    }
+                ]
+            }
     """.trimIndent()
+    val request = Request.Builder().url(post)
+        .addHeader("Content-type","application/json")
+        .addHeader("Authorization", "Bearer $api")
+        .post(requestBody.toRequestBody("application/json".toMediaTypeOrNull()))
+        .build()
+
+    client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("error","API failed", e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                if(body != null){
+                    Log.v("data", body)
+                }else{
+                    Log.v("data", "empty")
+                }
+            }
+        }    )
     }
 
 
